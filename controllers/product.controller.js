8870d4628362b934bc4d9993fdad369a100104e9
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-04-15 15:46:57 -0500

    Add files via upload

---
+++ controllers/product.controller.js
@@ -0,0 +1,47 @@
+/*
+
+create
+findAll
+findOne
+update
+delete
+deleteAll
+
+*/
+
+const db = require("../models");
+const Product = db.product;
+const User = db.user;
+
+exports.create = (req, resp) =>
+{
+
+};
+
+exports.findAll = (req, resp) =>
+{
+
+};
+
+exports.findOne = (req, resp) =>
+{
+
+};
+
+exports.update = (req, resp) =>
+{
+
+};
+
+exports.deleteOne = (req, resp) =>
+{
+
+};  
+
+exports.deleteAll = (req, resp) =>
+{
+
+};
+
+
+
